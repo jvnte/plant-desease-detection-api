e6961bf9a146f9e1d27bd44bfb54dc4d4114b11d
John Vicente <john.vicente@statworx.com>
2021-01-02 13:44:04 +0100

    Added image upload and display functionality

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,25 +1,43 @@
 <script>
+
+import { Col, Container, Row, Button, Form, FormGroup, Input, Label } from 'sveltestrap';
+
 	let out;
 	const url = "http://127.0.0.1:8000/predict";
 
-	function getPrediction() {
-	fetch(url, {
-		method: "POST",
-		headers: {
-			"Accept": "application/json",
-			"Content-Type": "application/json"
-		},
-		body: JSON.stringify({model_path: './models/mobile_net_100_20201207-090439/mobile_net_100_20201207-090439', img_path: './dataset/test/PotatoHealthy1.JPG'})
-	})
-	.then(d => d.json())
-    .then(d => {
-		out = d
-	});
-	}
+	function getPrediction(model_path = "./models/mobile_net_100_20201207-090439/mobile_net_100_20201207-090439", img_path = './dataset/test/PotatoHealthy1.JPG') {
+
+		const payload = {};
+		payload["model_path"] = model_path;
+		payload["img_path"] = img_path;
+
+		fetch(url, {
+			method: "POST",
+			headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(payload, ["message", "arguments", "type", "name"])
+		})
+		.then(d => d.json())
+    	.then(d => {
+			out = d
+		});
+		}
+
 </script>
 
 <main>
-	<button on:click={getPrediction}>Get a prediction</button>
+	<Container>
+		<Form>
+		<FormGroup>
+    		<Label for="exampleFile">File</Label>
+    		<Input type="file" name="file" id="exampleFile" oninput="predimg.src=window.URL.createObjectURL(this.files[0])" />
+			<img id="predimg" src="" width="200px" height="200px" alt = " "/>
+		</FormGroup>
+	</Form>
+		<Button on:click={getPrediction}>Get a prediction</Button>
+	</Container>
 	{#if out}
 	<h1>This is the outcome:</h1>
 	<p>Target: {out.target}</p>
